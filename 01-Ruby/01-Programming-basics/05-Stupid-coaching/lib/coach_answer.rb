616f39881ede22dc6fa4f0363932763d691d158b
Victor Avella <victorhugo.avellalemus@gmail.com>
2026-03-07 13:04:09 -0500

    finished

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,22 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message == "I am going to work right now!"
+    ""
+  elsif your_message.include?("?")
+    "Silly question, get dressed and go to work!"
+  else
+    "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours!
+  # TODO: return coach answer to your_message, with additional custom rules of yours !
+  answer = coach_answer(your_message)
+  if answer == "" || your_message == "I AM GOING TO WORK RIGHT NOW!"
+    ""
+  elsif your_message.upcase == your_message
+    "I can feel your motivation! #{answer}"
+  else
+    answer
+  end
 end
