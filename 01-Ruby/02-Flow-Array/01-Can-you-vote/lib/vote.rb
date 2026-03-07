197bf4570b1dbf72841a12d971e24a757c0e5292
Victor Avella <victorhugo.avellalemus@gmail.com>
2026-03-07 13:20:44 -0500

    finished

--- 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
+++ 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
@@ -1,4 +1,9 @@
 def allowed_to_vote?(age)
   # TODO: return (not print!) a boolean stating whether `age` is old enough to vote
   # NOTE: Use an if/else statment
+  if age >= 18
+    true
+  else
+    false
+  end
 end
