ace0210bc5a3df51eccc2ce81abb06e318c73c47
Victor Avella <victorhugo.avellalemus@gmail.com>
2026-03-17 17:40:42 -0500

    finished

---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/interface.rb
@@ -0,0 +1,7 @@
+require_relative "sport"
+
+futbol = Sport.new("Futbol", 11)
+ultimate_frisbee = Sport.new("Ultimate Frisbee", 7)
+
+p futbol
+p ultimate_frisbee
---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/sport.rb
@@ -0,0 +1,8 @@
+class Sport
+  attr_reader :name, :players
+
+  def initialize(name, players)
+    @name = name
+    @players = players
+  end
+end
