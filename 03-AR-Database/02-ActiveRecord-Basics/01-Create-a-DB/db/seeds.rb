4ae5da6332c0c10fe6251f2ba47b70945c634cda
Victor Avella <victorhugo.avellalemus@gmail.com>
2026-03-21 19:01:09 -0500

    finished

--- 03-AR-Database/02-ActiveRecord-Basics/01-Create-a-DB/db/seeds.rb
+++ 03-AR-Database/02-ActiveRecord-Basics/01-Create-a-DB/db/seeds.rb
@@ -1,5 +1,17 @@
 puts "Seeding..."
 
 # TODO: add some code to fill the database with useful universal data
+#
+
+characters = [
+  { name: "Alice", age: 30 },
+  { name: "Bob", age: 25 },
+  { name: "Charlie", age: 35 }
+]
+
+
+characters.each do |character_data|
+  Character.create(character_data)
+end
 
 puts "Done."
